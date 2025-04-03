08e2a0006fbe2c735e8fe822dbce064cd1dad577
drmy1 <62697911+drmy1@users.noreply.github.com>
2025-04-03 18:01:44 +0200

    added cname_gen

---
+++ CNAME_gen/main.py
@@ -0,0 +1,53 @@
+import secrets
+import string
+
+
+class AlreadyInDB(Exception):
+    pass
+
+
+def generator(digit=6) -> None:
+    # TODO if secret in db, call generator() again and check again.
+
+    cname = "".join(
+        secrets.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase)
+        for _ in range(digit)
+    )
+    try:
+        check_for_collision(cname)
+        save_secret(cname)
+
+    except AlreadyInDB:
+        generator()
+
+
+def check_for_collision(cname) -> None:
+    # TODO connect to db and check for dublicates
+    # try:
+    # if cname in db:
+    #     raise AlreadyInDB
+    pass
+
+
+# TODO change saving location
+def save_secret(cname) -> None:
+    try:
+        with open("CNAME.txt", "w") as f:
+            f.write(cname)
+    except Exception as e:
+        print(f"Error saving cname: {e}")
+    # finally:
+    #     send()
+
+
+def send():
+    # TODO connect to proxy somehow or sand some API request somewhere with cname, WG-IP and wg_pubkey.
+    # TODO Proxyserverer already has a automatic script for updating NGINX and WG config
+
+    # TODO send cname to DB (can be done in the same step as above)
+
+    pass
+
+
+if __name__ == "__main__":
+    generator()
